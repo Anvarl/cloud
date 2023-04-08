e71cf77de23d1e5e4c23545c54349df3b9e87583
Anvarl <anvaraidarbekov@gmail.com>
2023-04-08 13:11:48 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 2,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+    });
+});
